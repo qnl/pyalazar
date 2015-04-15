c25aaa77111bb3379967b2c7c6897477b97a6823
Chris Macklin <chris.macklin@berkeley.edu>
2015-04-15 16:34:28 -0700

    More test

--- tests/test_alazar.py
+++ tests/test_alazar.py
@@ -1,7 +1,13 @@
-from alazar.alazar import *
+import alazar.alazar as alz
 
-
+# nose module setup
+def setup():
+	boards = alz.get_systems_and_boards()
 
 class TestAlazar(object):
+
+	def test_systems_available():
+		assert boards
+
 	def test():
 		pass
