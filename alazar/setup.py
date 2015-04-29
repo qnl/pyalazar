05040cd3c50e2436e5e40148a9640f730478d11d
Chris Macklin <chris.macklin@berkeley.edu>
2015-04-29 10:13:34 -0700

    Removed unnecessary library path, added 64-bit vs. 32-bit includes.

--- alazar/setup.py
+++ alazar/setup.py
@@ -16,13 +16,19 @@ config = {
     'name': 'pyalazar'
 }
 
-# setup(ext_modules = cythonize(['alazar_hello_world.pyx']))
+alazar_SDK_path = "C:\\AlazarTech\\ATS-SDK\\6.2.0\\"
+
+# 32-bit:
+# library_path = 'Samples\\Library\\Win32'
+# 64-bit:
+library_path = 'Samples\\Library\\x64'
+
 
 alazar = Extension('alazar',
-                  sources = ['alazar.pyx'],
-                  include_dirs = ['C:\\AlazarTech\\ATS-SDK\\6.2.0\\Samples\\Include'],
-                  libraries = ['ATSApi'],
-                  library_dirs = ['C:\\AlazarTech\\ATS-SDK\\6.2.0\\Samples\\Library\\x64','C:\\AlazarTech\\ATS-SDK\\6.2.0\\Samples_CSharp\\'],)
+                  sources = ['alazar.pyx',],
+                  include_dirs = [alazar_SDK_path + 'Samples\\Include',],
+                  libraries = ['ATSApi',],
+                  library_dirs = [alazar_sdk_path + library_path, ],)
 
 setup(ext_modules = cythonize([alazar]),
       include_dirs=[numpy.get_include()],
